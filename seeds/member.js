0aaea60046fcbb25af80f366148ec9de1bfa574a
Red Monzon <redkmonzon30@gmail.com>
2024-02-05 08:39:54 -0600

    Add seed.js that contains an object with all of the JoinKoknekt member names

---
+++ seeds/member.js
@@ -0,0 +1,23 @@
+const member = [
+  {
+    name: "Red"
+  },
+  {
+    name: "Pawanjeet"
+  },
+  {
+    name: "Taiye"
+  },
+  {
+    name: "Isha"
+  },
+  {
+    name: "Jian"
+  },
+  {
+    name: "Garvit"
+  },
+  {
+    name: "Salim"
+  },
+];
